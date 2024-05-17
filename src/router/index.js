e5a8eb0d6b8a492b7d103faf9178ed2179e44a4b
SyifaniKurnia <127850651+SyifaniKurnia@users.noreply.github.com>
2024-05-17 21:28:28 +0700

    index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,13 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Todolist from "../views/Todolist.vue";
-import Postlist from "../views/Postlist.vue";
+import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
-      name: "todo",
-      component: Todolist,
-    },
-    {
-      path: "/post",
-      name: "post",
-      component: Postlist,
+      name: "home",
+      component: HomeView,
     },
   ],
 });
